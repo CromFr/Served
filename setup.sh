b2b3d7ef90cb606295faa0c75f6d636b0053b0ff
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2015-03-14 16:47:50 +0100

    Fix setup

--- setup.sh
+++ setup.sh
@@ -2,16 +2,16 @@
 
 #JQuery
 wget https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
-mkdir Public/jquery
-mv jquery.min.js Public/jquery
+mkdir public/jquery
+mv jquery.min.js public/jquery
 
 #ShowdownJS
 wget https://cdnjs.cloudflare.com/ajax/libs/showdown/0.3.1/showdown.min.js
-mkdir Public/showdown
-mv showdown.min.js Public/showdown
+mkdir public/showdown
+mv showdown.min.js public/showdown
 
 #Bootstrap
 wget https://github.com/twbs/bootstrap/releases/download/v3.3.1/bootstrap-3.3.1-dist.zip
 unzip bootstrap-3.3.1-dist.zip
 rm bootstrap-3.3.1-dist.zip
-mv dist/ Public/bootstrap
+mv dist/ public/bootstrap
